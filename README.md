605fb4085818e0ff7745a2462dd21062e437ad78
Vishal Bhardwaj <vishalbhardwaj0571@gmail.com>
2026-05-25 18:46:16 +0530

    LoginAPI, JWT

---
+++ README.md
@@ -0,0 +1,8 @@
+## 02-Login-and-register-usser-complete-so-far
+✅ Spring Boot project setup
+✅ MongoDB Atlas connected
+✅ User model
+✅ UserRepository
+✅ Register with BCrypt password hashing
+✅ Login with password verification
+✅ JWT token generation
---
+++ intervi/src/main/java/com/intervi/resume/security/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.intervi.resume.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    @Value("${app.jwt.expire}")
+    private long expire;
+
+    public String generateToken(String email) {
+
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expire))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    public String getEmailFromToken(String token) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    
+    public boolean isTokenValid(String token) {
+        try {
+            getEmailFromToken(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
