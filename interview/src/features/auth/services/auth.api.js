e61ebb6bcdcc0ce352d806f12e05b478f083a09b
Vishal Bhardwaj <vishalbhardwaj0571@gmail.com>
2026-05-26 21:11:00 +0530

    04-axios-AuthPage

---
+++ interview/src/features/auth/services/auth.api.js
@@ -0,0 +1,50 @@
+import api from "./axios";
+
+export const register = async (username, email, password) => {
+  try {
+    const response = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
+      return response.data;
+  } catch (error) {
+    console.log(err);
+  }
+  
+};
+
+export const login = async (email, pasword) => {
+  try {
+    const response = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
+      localStorage.setItem("token", response.data.token);
+       return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+ 
+};
+
+export const logout = async () => {
+  try {
+    const response = await api.post("/api/auth/logout");
+      localStorage.removeItem("token");
+        return response.data;
+  } catch (error) {
+    console.log(err);
+    }
+  
+};
+
+export const getMe = async () => {
+  try {
+      const response = await api.get("/api/auth/me");
+       return response.data;
+  } catch (error) {
+    console.log(err);
+  }
+ 
+};
